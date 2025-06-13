0f049dc11a1e822b9f2afffc43c63786ce02aaff
Michelle Deeney <michelledeeney@proton.me>
2025-06-13 16:54:58 +0100

    added base url to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './stylesheets/index.css'
 import App from './App.tsx'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/bench-mark-web/">
+      <Routes>  
+        <Route path="*" element = {<App/>}/>
+      </Routes>    
+    </BrowserRouter>
+  </StrictMode>
+);
