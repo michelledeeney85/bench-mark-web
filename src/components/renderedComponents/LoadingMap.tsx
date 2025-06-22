f5b130ac9fc18be3dc7c7d799ee0e5c5385501a2
Michelle <michelledeeney@proton.me>
2025-06-22 12:33:33 +0100

    Update LoadingMap.tsx

--- src/components/renderedComponents/LoadingMap.tsx
+++ src/components/renderedComponents/LoadingMap.tsx
@@ -1,7 +1,7 @@
 const LoadingMap = () => {
     return(<div className="mapview-loading">
             <h2>Loading map...</h2>
-            <h4>Bear with us, getting bench data from OpenStreetMap...</h4>
+            <h4>Getting bench data from OpenStreetMap...</h4>
             <p>(Also - make sure you have location enabled in your browser.)</p>
           </div>);
 };
